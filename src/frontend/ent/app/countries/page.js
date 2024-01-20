dac08bad518774b34cb5a6e5f2fbebeef146c6fa
M2ndess <121449441+M2ndess@users.noreply.github.com>
2024-01-20 14:53:05 +0000

    Front-End Countries

--- src/frontend/ent/app/countries/page.js
+++ src/frontend/ent/app/countries/page.js
@@ -1,3 +1,31 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import crudAPI from '../crud/crudAPI';
+
 export default function CountriesPage() {
-    return <main><b>Countries Page</b>: Work in progress</main>;
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await crudAPI().GET('/countries');
+        setCountries(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar teams:', error);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+
+  return (
+    <main>
+      <b>Countries Page</b>:
+      <ul>
+        {countries.map((country) => (
+          <li key={country.id}>{country.name}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
